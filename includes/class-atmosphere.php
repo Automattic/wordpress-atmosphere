3388ece2cbda8ca4118698f8e0c0b7f40cc5c882
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-03-21 14:16:09 +0100

    Remove integrations references from content-parser branch
    The integrations directory lives on trunk; this branch should not
    reference it in the classmap or Atmosphere init.

--- includes/class-atmosphere.php
+++ includes/class-atmosphere.php
@@ -13,7 +13,6 @@ use Atmosphere\OAuth\Client;
 use Atmosphere\Transformer\Document;
 use Atmosphere\Transformer\Publication;
 use Atmosphere\Transformer\TID;
-use Atmosphere\Integrations\Load;
 use Atmosphere\WP_Admin\Admin;
 
 /**
@@ -43,9 +42,6 @@ class Atmosphere {
 
 		// JSON preview for AT Protocol records.
 		\add_action( 'template_redirect', array( $this, 'preview' ) );
-
-		// Plugin integrations.
-		Load::init();
 
 		// Post lifecycle hooks.
 		\add_action( 'transition_post_status', array( $this, 'on_status_change' ), 10, 3 );
