530ab61b698e08888113c2d92ce74be1128cb29f
Brandon Kraft <public@brandonkraft.com>
2026-05-21 16:06:22 -0500

    Fix ATMOSPHERE_VERSION constant and release-script regex (#80)

--- atmosphere.php
+++ atmosphere.php
@@ -19,7 +19,7 @@ namespace Atmosphere;
 
 \defined( 'ABSPATH' ) || exit;
 
-\define( 'ATMOSPHERE_VERSION', '1.0.0' );
+\define( 'ATMOSPHERE_VERSION', '1.1.0' );
 \define( 'ATMOSPHERE_PLUGIN_DIR', \plugin_dir_path( __FILE__ ) );
 \define( 'ATMOSPHERE_PLUGIN_URL', \plugin_dir_url( __FILE__ ) );
 \define( 'ATMOSPHERE_PLUGIN_FILE', __FILE__ );
