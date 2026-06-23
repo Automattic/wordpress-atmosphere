e985c7f3927f19ca2c794c8ebab4c294de3cbb97
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-06-23 17:31:09 +0200

    Update: Brand the editor panel as ATmosphere with the Bluesky butterfly
    Rename the document panel from "Bluesky" to "ATmosphere" (matching the admin
    menu and settings page) and add the Bluesky butterfly logo after the title.
    Relabel the share toggle to "Share this post" with help that names ATmosphere,
    since the toggle controls all sharing (Bluesky post + standard.site document),
    not just Bluesky. The custom-text field stays Bluesky-specific.

--- build/editor-plugin/plugin.asset.php
+++ build/editor-plugin/plugin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => '9ba8f41d31623bf94f08');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => 'f503153ee4e34dc86e56');
